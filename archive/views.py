b3deeeee86aadbac1ac236d1fa9993ebd1e1721c
Ross Jones <ross@servercode.co.uk>
2015-06-02 14:33:33 +0100

    Mockup of archive navigation

--- archive/views.py
+++ archive/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return redirect('archive:view', path='')
 
+def resource_view(request, id):
+    ctx = {"resource": {"id": id, "name": "A test name", "collection":{"name": "data"}}}
+    return render(request, 'archive/resource.html', ctx)
+
 def navigate(request, path):
 
     parts = path.split('/')
@@ -20,6 +24,11 @@ def navigate(request, path):
             {"name": "data", "path": "data"},
             {"name": "something", "path": "data/something"},
             {"name": "something-else", "path": "data/something/something-else"},
+        ]
+        ctx['collection']['resources'] = [
+            {"name": "Some data file", "type": "XLS", "id": "X"},
+            {"name": "A CSV file", "type": "CSV", "id": "Y"},
+            {"name": "An XML file", "type": "XML", "id": "Z"},
         ]
 
     return render(request, 'archive/index.html', ctx)
