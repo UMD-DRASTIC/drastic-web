947e09945f64d2e2f2736230228af47fd34cd835
Ross Jones <ross@servercode.co.uk>
2015-06-22 16:32:12 +0100

    Show metrics from nodes

--- indigo_ui/templatetags/ui_tags.py
+++ indigo_ui/templatetags/ui_tags.py
@@ -16,6 +16,16 @@ def percentage(parser, token):
         )
     return PercentageNode(total, used)
 
+@register.tag(name="percentage_color")
+def percentage_color(parser, token):
+    try:
+        tag_name, total, used = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            "%r tag requires exactly two arguments" % token.contents.split()[0]
+        )
+    return PercentageColorNode(total, used)
+
 
 class PercentageNode(template.Node):
     def __init__(self, total, used):
@@ -26,8 +36,26 @@ class PercentageNode(template.Node):
         try:
             actual_total = self.total.resolve(context)
             actual_used = self.used.resolve(context)
-            print actual_used, actual_total
         except template.VariableDoesNotExist:
             return ''
 
         return 100 / (actual_total / actual_used)
+
+class PercentageColorNode(template.Node):
+    def __init__(self, total, used):
+        self.total = template.Variable(total)
+        self.used   = template.Variable(used)
+
+    def render(self, context):
+        try:
+            actual_total = self.total.resolve(context)
+            actual_used = self.used.resolve(context)
+        except template.VariableDoesNotExist:
+            return ''
+
+        pc = (100 / (actual_total / actual_used))
+        if pc < 50:
+            return 'success'
+        if pc < 75:
+            return 'warning'
+        return 'danger'
