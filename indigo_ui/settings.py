896aec0c4d356f1ba8558b3124115c7d3ed21c78
Ross Jones <ross@servercode.co.uk>
2015-07-23 13:34:20 +0100

    Remove mention of postgres from settings

--- indigo_ui/settings.py
+++ indigo_ui/settings.py
@@ -97,11 +97,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'indigo',
-        'HOST': '127.0.0.1',
-        'USER': 'indigo',
-        'PASS': 'indigo'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join("/tmp/", 'db.sqlite3'),
     },
 }
 
