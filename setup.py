801343d5903bad13165aec14b32cb0868c9a0a4a
Jerome Fuselier <jerome.fuselier@free.fr>
2016-02-05 08:39:32 +0100

    Allow packaging of the source with sdist

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""Setup for Indigo-Web
+
+Copyright 2015 Archive Analytics Solutions
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+from distutils.core import setup
+from setuptools import setup
+
+
+setup(
+    name='indigo-web',
+    version="1.0",
+    description='Indigo web ui',
+    extras_require={},
+    long_description="Django web ui for Indigo",
+    author='Archive Analytics',
+    maintainer_email='@archiveanalytics.com',
+    license="Apache License, Version 2.0",
+    url='https://bitbucket.org/archivea/indigo-web',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: System :: Archiving"
+    ],
+)
