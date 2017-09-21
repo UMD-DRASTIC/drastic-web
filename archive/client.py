ea31044952ae919d17dbc55e2763d620421bf923
Gregory Jansen <gregory.jansen@gmail.com>
2017-09-21 13:24:48 -0400

    upgrades Django

--- archive/client.py
+++ archive/client.py
@@ -4,16 +4,19 @@ This client class is used to retrieve information from the agent using
 the CDMI interface that it exposes.
 
 """
-__copyright__ = "Copyright (C) 2016 University of Maryland"
-__license__ = "GNU AFFERO GENERAL PUBLIC LICENSE, Version 3"
-
 
 from libcdmi import cdmi
 from django.conf import settings
 
+
+__copyright__ = "Copyright (C) 2016 University of Maryland"
+__license__ = "GNU AFFERO GENERAL PUBLIC LICENSE, Version 3"
+
+
 def get_default_client():
     cfg = settings.CDMI_SERVER
-    return Client(cfg["endpoint"], cfg["username"], cfg["password"] )
+    return Client(cfg["endpoint"], cfg["username"], cfg["password"])
+
 
 class Client(object):
 
@@ -40,4 +43,3 @@ class Client(object):
 
     def get_resource_content(self, full_path):
         return self.conn.blob_proxy.read(full_path, cdmi_object=False)
-
